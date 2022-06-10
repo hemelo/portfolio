9225dd416ab7024ffcf33c855673a883133b08a3
Henrique Melo <hmelo2509@gmail.com>
2022-06-10 21:00:09 +0000

    fix: express certificates route

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,18 @@
 const app = require("express")();
 const unirest = require("unirest");
 
-app.get("/api/certificates", async function (req, res) {
-  const request = await unirest
+app.get("/api/certificados", function (req, res) {
+  const request = unirest
     .get("https://cursos.alura.com.br/user/hmelo2509/fullCertificate/03b702831d58f6fbf691a6bddc2630d4")
     .headers({
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": "true",
     });
   
-  console.log(request)
-  res.json(request)
+  request.end(function (response) {
+    if (response.error) throw new Error(response.error);
+    res.json(response.body);
+  });
 });
     
 module.exports = app;
